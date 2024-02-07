7dfd28db398823c17cfb1587115ffb4d3119645b
OGS-1418 <156859753+OGS-1418@users.noreply.github.com>
2024-02-07 12:43:37 -0400

    Agregar archivos de proyecto.

---
+++ charts_carrousel/Program.cs
@@ -0,0 +1,31 @@
+using charts_carrousel.Client.Pages;
+using charts_carrousel.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(charts_carrousel.Client._Imports).Assembly);
+
+app.Run();
